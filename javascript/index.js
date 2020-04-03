f4743b6abcf3896765253d38ebd1e052acb5a283
ROUSHAN KUMAR <52878060+theRoushan@users.noreply.github.com>
2020-04-03 19:56:11 +0530

    updated login and signup pates authenticatio status with sweet alert 2

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,27 @@
+function redirect_to_homepage() {
+    location.replace("index.html")
+};
+
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         // User is signed in.
-
+        document.getElementById("sign-in").style({ display: none });
+        document.getElementById("sign-up").style({ display: none });
 
         var user = firebase.auth().currentUser;
 
         if (user != null) {
-
             var email_id = user.email;
-            document.getElementById("welcome-status").innerHTML = "Welcome User : " + email_id;
-
+            Swal.fire({
+                    position: 'top-end',
+                    title: "Welcome User : " + email_id,
+                    showConfirmButton: false,
+                    timer: 1500,
+                    width: 300,
+                    height: 30
+                })
+                //redirects the user to haomepage if the user is already loggedin.
+            redirect_to_homepage();
         }
 
     } else {
